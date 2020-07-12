c272bacb2b76458fa5590cca592d065d98691a97
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2020-07-12 17:04:34 +0300

    simplify Home.vue view

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home sdf</router-link> |
+      <router-link to="/">Home </router-link> |
       <router-link to="/play">Play (Offline)</router-link>
     </div>
     <router-view />
