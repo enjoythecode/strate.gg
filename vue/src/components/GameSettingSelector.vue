eadb9bd547c4b1fbce493a6a27da05d56abc2eb7
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2020-07-12 15:22:10 +0300

    modify template to learn about project structure

---
+++ vue/src/components/GameSettingSelector.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} Settings Selector</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameSettingSelector",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+}
+</style>
---
+++ vue/src/views/PlayOffline.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="playOffline">
+    <h1>This is the play offline page</h1>
+    <GameSettingSelector />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GameSettingSelector from "@/components/GameSettingSelector.vue";
+
+export default {
+  name: "PlayOffline",
+  components: {
+    GameSettingSelector
+  }
+};
+</script>
