27399cee4cae66d8fd43d37df504834ad8f90912
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2020-07-12 17:13:28 +0300

    move the different start game buttons to the PlayOffline.vue view from a child component

--- vue/src/components/GameSettingSelector.vue
+++ vue/src/components/GameSettingSelector.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="hello">
-    <p>Settings Selector</p>
-        <button @click="start_game(4,0)">4x4, config 1</button>
-        <button @click="start_game(6,0)">6x6, config 1</button>
-        <button @click="start_game(10,0)">10x10, config 1</button>
+    
   </div>
 </template>
 
--- vue/src/views/PlayOffline.vue
+++ vue/src/views/PlayOffline.vue
@@ -2,17 +2,21 @@
   <div class="playOffline">
     <h1>Play (Offline)</h1>
     <GameSettingSelector />
+    <p>Settings Selector</p>
+    <button @click="start_game(4,0)">4x4, config 1</button>
+    <button @click="start_game(6,0)">6x6, config 1</button>
+    <button @click="start_game(10,0)">10x10, config 1</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import GameSettingSelector from "@/components/GameSettingSelector.vue";
+//import GameSettingSelector from "@/components/GameSettingSelector.vue";
 
 export default {
   name: "PlayOffline",
   components: {
-    GameSettingSelector
+    //GameSettingSelector
   }
 };
 </script>
