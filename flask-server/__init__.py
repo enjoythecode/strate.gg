c97602bf36240b043a68371b8128843b18cba405
enjoythecode <sinan.yumurtaci@gmail.com>
2021-10-01 18:12:29 -0400

    remove some old code

--- flask-server/__init__.py
+++ flask-server/__init__.py
@@ -1,37 +1,20 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
 import os
 
-from flask import Flask
+games = {}
+users = {}
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    app.config.from_mapping(SECRET_KEY='dev')
 
     @app.route('/')
     def serve_index():
         return app.send_static_file('index.html')
 
+    # Vue front-end, WIP
     @app.route('/game.html')
     def serve_game():
         return app.send_static_file('game.html')
