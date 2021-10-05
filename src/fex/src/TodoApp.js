825a7e25ee98a2cd4e3f1a0c6ade5af6205f0382
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2021-10-05 19:29:54 -0400

    WIP: create-react-app

---
+++ src/fex/src/TodoApp.js
@@ -0,0 +1,60 @@
+import * as React from "react"
+import { render } from "react-dom"
+import { observer } from "mobx-react-lite"
+import { makeObservable, observable, computed, action } from "mobx"
+
+class Todo {
+    id = Math.random()
+    title = ""
+    finished = false
+
+    constructor(title) {
+        makeObservable(this, {
+            title: observable,
+            finished: observable,
+            toggle: action
+        })
+        this.title = title
+    }
+
+    toggle() {
+        this.finished = !this.finished
+    }
+}
+
+class TodoList {
+    todos = []
+    get unfinishedTodoCount() {
+        return this.todos.filter(todo => !todo.finished).length
+    }
+    constructor(todos) {
+        makeObservable(this, {
+            todos: observable,
+            unfinishedTodoCount: computed
+        })
+        this.todos = todos
+    }
+}
+
+const TodoListView = observer(({ todoList }) => (
+    <div>
+        <ul>
+            {todoList.todos.map(todo => (
+                <TodoView todo={todo} key={todo.id} />
+            ))}
+        </ul>
+        Tasks left: {todoList.unfinishedTodoCount}
+    </div>
+))
+
+const TodoView = observer(({ todo }) => (
+    <li>
+        <input type="checkbox" checked={todo.finished} onClick={() => todo.toggle()} />
+        {todo.title}
+    </li>
+))
+
+const store = new TodoList([new Todo("Get Coffee"), new Todo("Write simpler code")])
+const TodoApp = () => (<TodoListView todoList={store} />)
+
+export default TodoApp
