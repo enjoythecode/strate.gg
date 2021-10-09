0493c6ed07b905e55465e78ffeece93629980868
enjoythecode <sinan.yumurtaci@gmail.com>
2021-10-08 22:45:38 -0400

    WIP: Amazons: display text as pieces, basic move handling

--- src/fex/src/Games/Amazons.js
+++ src/fex/src/Games/Amazons.js
@@ -1,4 +1,4 @@
-import { makeObservable, observable, computed, action, flow } from "mobx"
+import { makeObservable, computed, observable, action } from "mobx"
 import { observer } from "mobx-react"
 import React from "react"
 
@@ -25,50 +25,104 @@ const whiteCell = {
     "backgroundColor": "#F0D9B5"
 }
 
-let startingBoards = {
-    "10_0": [[0, 0, 0, 2, 0, 0, 2, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 1, 0, 0, 0]],
-    "6_0": [
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 2],
-        [2, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0]
-    ],
-    "4_0": [
-        [0, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 2, 0]
-    ]
-
-}
-
-let initializeBoard = (config) => {
+const initializeBoard = (config) => {
+    let startingBoards = {
+        "10_0": [[0, 0, 0, 2, 0, 0, 2, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 1, 0, 0, 0]],
+        "6_0": [
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 2],
+            [2, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0]
+        ],
+        "4_0": [
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 2, 0]
+        ]
+    
+    }
     if(!("size" in config && "variation" in config)){console.log("-1-", config); return false;}
     let key = config["size"].toString() + "_" + config["variation"].toString()
     if(! key in startingBoards) return false;
     return startingBoards[key]
 }
 
+const getCellImage = (id) => {
+    switch (id) {
+        case 0:
+            return null
+        case 1:
+            return "White"
+        case 2:
+            return "Black"
+        case 3:
+            return "Burnt off"
+    }
+}
+
 class Amazons {
+
     constructor(config){
+        makeObservable(this,{
+            board: observable,
+            selection: observable,
+            clickCell: action,
+            selectionFrom:  computed,
+            selectionTo: computed,
+            selectionShoot: computed
+        })
+
         this.config = config
-        this.sss = "ssdsf"
         this.board = initializeBoard(config)
         if(this.board === false){
             console.log("invalid board config!")
-        }   
+        }
     }
+
+    board = null
+    selection = [null, null, null]; // selected cells; [from, to, shoot]
+
+    clickCell = (c) => {
+        console.log("notvalud")
+        if(this.isValidClick(c)){
+            console.log('valid')
+            if(this.selectionFrom === null){
+                this.selection[0] = c
+            }else if(this.selectionTo === null){
+                this.selection[1] = c
+            }else{
+                this.selection[2] = c
+                this.sendMove(this.selection)
+                this.selection = [null, null, null]
+            }
+        }
+    }
+
+    get selectionFrom () {return this.selection[0]}
+    get selectionTo () {return this.selection[1]}
+    get selectionShoot () {return this.selection[2]}
+
+    isValidClick(c){
+        let source = this.selectionTo === null ? this.selectionFrom : this.selectionTo
+        return true
+    }
+
+    sendMove(sel) {
+        console.log("implement sending moves!")
+    }
+
 }
 
 const AmazonsView = observer(class _ extends React.Component{
@@ -81,14 +135,24 @@ const AmazonsView = observer(class _ extends React.Component{
             for(let row of this.props.challenge.game_state.board){
                 for(let cell of row){
                     let colorCss = (counter + rowCounter) % 2 == 0 ? blackCell : whiteCell;
-                    boardCells.push(<div style={{...cellCss, ...colorCss}} key={counter}>{counter}</div>)
-                    counter++
+                    boardCells.push(<div   
+                                        style={{...cellCss, ...colorCss}}
+                                        key={counter}
+                                        onClick={this.props.challenge.game_state.clickCell.bind(null,counter)}>
+                                            {getCellImage(cell)}
+                                    </div>)
+                    counter += 1;
                 }
                 rowCounter++
             }
             return (
                 <div style={{"height": "300px"}}>
-                    <h3>inheritance! {this.props.challenge.game_state.sss} {this.props.challenge.game_state.board === false}</h3>
+                    <h3>inheritance!{this.props.challenge.game_state.board === false}</h3>
+                    <h4>
+                        from {this.props.challenge.game_state.selectionFrom}
+                        to {this.props.challenge.game_state.selectionTo}
+                        shoot {this.props.challenge.game_state.selectionShoot}
+                    </h4>
                     <div style={boardCss}>
                         {boardCells}
                     </div>
