6674d8246700dfac192efbd5303fcf3b32e0cd11
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2021-10-09 23:15:34 -0400

    fix regression: fix amazons: is_valid_move()

--- README.md
+++ README.md
@@ -39,7 +39,6 @@ In rough order of priority;
 1. Front-end refactor to MobX + React
     - Regressions:
         - Move indicators during click for Amazons
-            - Implement is_valid_move()
         - Images instead of text on the Amazons board.
     - Upgrades:
         - Upgrade current game UI architecture for games to be composed of board and pieces on arbitrary locations (that are relative to the container!)
--- src/fex/src/Games/Amazons.js
+++ src/fex/src/Games/Amazons.js
@@ -82,6 +82,7 @@ class Amazons {
             selection: observable,
             clickCell: action,
             process_new_move: action,
+            resetSelection: action,
             selectionFrom:  computed,
             selectionTo: computed,
             selectionShoot: computed
@@ -95,28 +96,66 @@ class Amazons {
     board = null
     selection = [null, null, null]; // selected cells; [from, to, shoot]
 
+    resetSelection = () => {
+        this.selection = [null, null, null];
+    }
+
     clickCell = (c) => {
-        if(this.isValidClick(c)){
-            if(this.selectionFrom === null){
+        if(this.selectionFrom === null){ // from click
+            if(this.board[Number(c[0])][Number(c[1])] == this.turn){
                 this.selection[0] = c
-            }else if(this.selectionTo === null){
+            }
+        }else if(this.selectionTo === null){ // to click
+            if(this.cell_can_reach(this.selectionFrom, c)){
                 this.selection[1] = c
             }else{
+                this.resetSelection()
+            }
+        }else{ // shoot click
+            if(this.cell_can_reach(this.selectionTo, c, this.selectionFrom)){
                 this.selection[2] = c
                 this.challenge.send_move(this.formatMoveForSend())
                 this.selection = [null, null, null]
+            }else{
+                this.resetSelection()
             }
         }
+    }
+
+    cell_can_reach = (from, to, ignore) => {
+        let from_x = Number(from.toString()[0])
+        let from_y = Number(from.toString()[1])
+        let to_x = Number(to.toString()[0])
+        let to_y = Number(to.toString()[1])
+        let d_x = to_x - from_x
+        let d_y = to_y - from_y
+    
+        if(d_x == 0 && d_y == 0) return false;
+        if(d_x != 0 && d_y != 0 && Math.abs(d_x) != Math.abs(d_y)) return false // on the diagonal, |x| must == |y|
+        
+        // normalize d_x, d_y to 1, 0, or -1. 
+        let step_x = d_x == 0 ? 0 : d_x / Math.abs(d_x) //the short-hand if prevents division by 0
+        let step_y = d_y == 0 ? 0 : d_y / Math.abs(d_y) //the short-hand if prevents division by 0
+        
+        let steps = Math.max(Math.abs(d_x), Math.abs(d_y)) // how many squares is between from and to?
+        
+        for (let i = 1; i <=steps; i++) {
+            let new_x = from_x + step_x * i
+            let new_y =from_y + step_y * i
+            if(this.board[new_x][new_y] != 0){
+                if (new_x.toString() + new_y.toString() != ignore){
+                    return false
+                }
+    
+            }
+        }
+    
+        return true
     }
 
     get selectionFrom () {return this.selection[0]}
     get selectionTo () {return this.selection[1]}
     get selectionShoot () {return this.selection[2]}
-
-    isValidClick(c){
-        let source = this.selectionTo === null ? this.selectionFrom : this.selectionTo
-        return true
-    }
 
     formatMoveForSend(){
         return {"from": this.selectionFrom,
@@ -149,19 +188,25 @@ const AmazonsView = observer(class _ extends React.Component{
             let counter = 0
             let rowCounter = 0;
             let cellCss = makeCellCss(this.props.challenge.game_state.board.length)
+            
             for(let row of this.props.challenge.game_state.board){
                 for(let cell of row){
+                    
                     let colorCss = (counter + rowCounter) % 2 === 0 ? blackCell : whiteCell;
-                    boardCells.push(<div   
-                                        style={{...cellCss, ...colorCss}}
-                                        key={counter}
-                                        onClick={this.props.challenge.game_state.clickCell.bind(null,rowCounter.toString() + (counter % this.props.challenge.game_state.board.length).toString())}>
-                                            {getCellImage(cell)}
-                                    </div>)
+                    let cellStringId = rowCounter.toString() + (counter % this.props.challenge.game_state.board.length).toString();
+                    boardCells.push(
+                        <div   
+                            style={{...cellCss, ...colorCss}}
+                            key={counter}
+                            onClick={this.props.challenge.game_state.clickCell.bind(null, cellStringId)}>
+                                {getCellImage(cell)}
+                        </div>)
                     counter += 1;
+
                 }
                 rowCounter++
             }
+
             return (
                 <div>
                     <h4>
@@ -179,7 +224,7 @@ const AmazonsView = observer(class _ extends React.Component{
         }else{
             return (
                 <div>
-                    <p>"Loading Challenge"</p>
+                    <p>Loading Challenge</p>
                 </div>
             )
         }
