ff1fd89a2d5263c57a4c199bad1922b7daebeead
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-03-03 18:33:04 +0100

    fix conftest socketio client to simulate an HTTP load with cookies first

--- be/tests/conftest.py
+++ be/tests/conftest.py
@@ -34,4 +34,8 @@ def runner(app):
 
 @pytest.fixture
 def socketio_client(app, client):
+    # emulates the real socketio usage where we load index first
+    # and then connect with socketio, and thus the server socketio
+    # has access to our cookie!
+    client.get("/")
     return socketio.test_client(app, flask_test_client=client)
