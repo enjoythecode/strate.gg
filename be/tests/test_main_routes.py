e195b7271d938554dcee3f154cb18df703b7a6f4
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-03-03 16:59:42 +0100

    test same cookie across requests

--- be/tests/test_main_routes.py
+++ be/tests/test_main_routes.py
@@ -25,7 +25,7 @@ def test_index_cookie_attributes(client):
 
     session_cookie = get_session_cookie_from_response(response)
 
-    cookie_attrs = parse_cookie(session_cookie)
+    cookie_attrs = get_attributes_of_cookie(session_cookie)
     cookie_value = cookie_attrs["session"]
 
     assert "Secure" in cookie_attrs
@@ -35,6 +35,17 @@ def test_index_cookie_attributes(client):
 
     assert isinstance(cookie_value, str)
     assert len(cookie_value) > 0
+
+
+@pytest.mark.usefixtures("client")
+def test_same_cookie_across_requests(client):
+    first_response = client.get("/")
+    second_response = client.get("/")
+
+    first_sess = get_session_value_from_response(first_response)
+    second_sess = get_session_value_from_response(second_response)
+
+    assert first_sess == second_sess
 
 
 def assert_response_200(response):
@@ -48,3 +59,13 @@ def assert_response_is_index_html(response):
 def get_session_cookie_from_response(response):
     cookies = response.headers.getlist("Set-Cookie")
     return next((cookie for cookie in cookies if "session" in cookie), None)
+
+
+def get_session_value_from_response(response):
+    session_cookie = get_session_cookie_from_response(response)
+    attrs = get_attributes_of_cookie(session_cookie)
+    return attrs["session"]
+
+
+def get_attributes_of_cookie(cookie):
+    return parse_cookie(cookie)
