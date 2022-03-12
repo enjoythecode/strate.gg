4568c2cafb934a8f609d57c8a7b20fb09b52dc28
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-03-12 14:53:48 +0100

    convert React Class component to functional components

--- fe/src/Components/MainPurposeComponents.js
+++ fe/src/Components/MainPurposeComponents.js
@@ -11,81 +11,67 @@ const shuffleArray = function (array) {
   return array;
 };
 
-const ConnectionWidget = observer(
-  class ConnectionWidget extends React.Component {
-    render() {
-      return (
-        <div style={{ position: "absolute", top: "4px", right: "8px" }}>
-          You're {RootStore.socket.connection_status}.
-          <b> {RootStore.socket.active_users}</b> online users!
-        </div>
-      );
-    }
-  }
+const ConnectionWidget = observer(() => (
+  <div style={{ position: "absolute", top: "4px", right: "8px" }}>
+    You're {RootStore.socket.connection_status}.
+    <b> {RootStore.socket.active_users}</b> online users!
+  </div>
+));
+
+const Header = () => (
+  <header>
+    <h1
+      style={{
+        paddingLeft: "20%",
+        fontFamily: "'Courier New', monospace",
+        margin: "0",
+      }}
+    >
+      strate.gg
+    </h1>
+    <ConnectionWidget />
+  </header>
 );
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <h1
-          style={{
-            paddingLeft: "20%",
-            fontFamily: "'Courier New', monospace",
-            margin: "0",
-          }}
-        >
-          strate.gg
-        </h1>
-        <ConnectionWidget />
-      </header>
+const Footer = () => {
+  // marquee is an easter egg, not shown in the UI
+  const marqueeArr = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Warning: Do not use <marquee> elements as they can create visual accessibility issues and are deprecated.",
+    "Comics I enjoy: xkcd",
+    <a href="https://xkcd.com/1112/">Here's why Chess is not on strate.gg</a>,
+  ];
+  let marqueeArrShuffled = shuffleArray(marqueeArr);
+  let marqueeTags = [];
+  for (let i = 0; i < marqueeArr.length; i++) {
+    marqueeTags.push(
+      <span style={{ paddingRight: "10em" }} key={i}>
+        {marqueeArrShuffled[i]}
+      </span>
     );
   }
-}
 
-class Footer extends React.Component {
-  render() {
-    // marquee is an easter egg, not shown in the UI
-    const marqueeArr = [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "Warning: Do not use <marquee> elements as they can create visual accessibility issues and are deprecated.",
-      "Comics I enjoy: xkcd",
-      <a href="https://xkcd.com/1112/">Here's why Chess is not on strate.gg</a>,
-    ];
-    let marqueeArrShuffled = shuffleArray(marqueeArr);
-    let marqueeTags = [];
-    for (let i = 0; i < marqueeArr.length; i++) {
-      marqueeTags.push(
-        <span style={{ paddingRight: "10em" }} key={i}>
-          {marqueeArrShuffled[i]}
-        </span>
-      );
-    }
-
-    return (
-      <footer>
-        <p style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+  return (
+    <footer>
+      <p style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+        <span>
+          A project by <a href="https://enjoythecode.com">Sinan</a> dedicated to
+          the play and exploration of abstract strategy board games.
+          <br />
           <span>
-            A project by <a href="https://enjoythecode.com">Sinan</a> dedicated
-            to the play and exploration of abstract strategy board games.
-            <br />
-            <span>
-              Take a look at the{" "}
-              <a href="https://github.com/enjoythecode/strate.gg">
-                source code.
-              </a>
-            </span>
+            Take a look at the{" "}
+            <a href="https://github.com/enjoythecode/strate.gg">source code.</a>
           </span>
+        </span>
 
-          {/*
+        {/*
             <marquee truespeed="True" scrolldelay="10" scrollamount="1">
                 {marqueeTags}
             </marquee>
           */}
-        </p>
-      </footer>
-    );
-  }
-}
+      </p>
+    </footer>
+  );
+};
 
 export { Header, Footer };
