0908317f5ade98e6586479b1b1dcc9d84be1b7fc
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-06-24 01:07:20 -0700

    User.getDisplayName

--- core/src/user.ts
+++ core/src/user.ts
@@ -3,6 +3,10 @@ export class User {
     isRegistered: boolean
 
     constructor(displayName : string = ""){
-        this.displayName = displayName;
+        this.displayName = displayName
+    }
+
+    getDisplayName() : string {
+        return this.displayName
     }
 }
--- core/tests/user.test.ts
+++ core/tests/user.test.ts
@@ -1,10 +1,16 @@
 import { User } from "../src/user"
 
+const SINAN = "sinan"
+
 describe("User", () => {
     it("can be initialized without arguments", () => {
         const user = new User();
     })
     it("can be initialized with a name", () => {
-        const sinan = new User("sinan");
+        const sinan = new User(SINAN);
+    })
+    it("gives access to the name it was given", () => {
+        const sinan = new User(SINAN);
+        expect(sinan.getDisplayName()).toBe(SINAN)
     })
 })
