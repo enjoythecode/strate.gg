f24faca28a0bd5193893c68ce6f5d581cae17851
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-06-24 01:18:23 -0700

    new Match class

---
+++ core/tests/match.test.ts
@@ -0,0 +1,9 @@
+import { Match } from "../src/match"
+
+describe("Match", () => {
+    describe("initalization without values", () => {
+        it("can be initialized", () => {
+            const match = new Match();
+        })
+    })
+})
